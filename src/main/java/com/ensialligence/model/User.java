10ebf95a8fbe9aa17f6b7c298acae6cae16c3d51
mohammed <med.eddaghal@gmail.com>
2021-02-21 23:42:11 +0100

    add methods (findUser,addUser)=>(LogIn,SignIn)

--- src/main/java/com/ensialligence/model/User.java
+++ src/main/java/com/ensialligence/model/User.java
@@ -2,31 +2,101 @@ package com.ensialligence.model;
 
 import java.io.Serializable;
 
+import com.google.protobuf.Timestamp;
+
 public class User implements Serializable {
-    private Long idUser;
-    private String userName;
-    private String passWd;
-    public User(Long idUser,String userName,String passWd) {
-        this.idUser=idUser;
-        this.userName=userName;
-        this.passWd=passWd;
-    }
-    public Long getIdUser() {
-        return idUser;
-    }
-    public void setIdUser(Long idUser) {
-        this.idUser = idUser;
-    }
-    public String getUserName() {
-        return userName;
-    }
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-    public String getPassWd() {
-        return passWd;
-    }
-    public void setPassWd(String passWd) {
-        this.passWd = passWd;
-    }
+    private Long id;
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String mail;
+    private String date_nais;
+    
+    public User() {
+		super();
+	}  
+    
+    public User(String username, String password, String firstname, String lastname, String mail,
+			String date_nais) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.mail = mail;
+		this.date_nais = date_nais;
+	}
+
+	public User(Long id, String username, String password, String firstname, String lastname, String mail,
+			String date_nais) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.mail = mail;
+		this.date_nais = date_nais;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getDate_nais() {
+		return date_nais;
+	}
+
+	public void setDate_nais(String date_nais) {
+		this.date_nais = date_nais;
+	}
+    
+    
 }
