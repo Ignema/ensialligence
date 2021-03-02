480d44b104adb8cb3970afb106d3e531266d972e
mohammed <med.eddaghal@gmail.com>
2021-03-02 20:30:26 +0100

    my last commit (ajout des methodes)

--- src/main/java/com/ensialligence/dao/UserDao.java
+++ src/main/java/com/ensialligence/dao/UserDao.java
@@ -1,5 +1,7 @@
 package com.ensialligence.dao;
 
+import java.util.ArrayList;
+
 import com.ensialligence.model.User;
 
 public interface UserDao {
@@ -20,9 +22,23 @@ public interface UserDao {
 	
 	/**
 	 * 
-	 * @param idUser the id of the user that we want to add to our friend's list
+	 * @param idUser1,idUser2 the id of the user that we want to add to our friend's list
 	 * @return false cas d'erreur d'ajout; true sinon
 	 */
-	boolean followUser(Long idUser);
+	boolean followUser(Long idUser1,Long idUser2);
+	
+	/**
+	 * 
+	 * @param idUser est l'id de l'utilisateur qu'on veut returner sa list des amis
+	 * @return list des amis de l'utilisateur "idUser"
+	 */
+	ArrayList<User> listFriends(Long idUser);
+
+	/**
+	 * 
+	 * @param idUser l'id de user qu'on cherche
+	 * @return l'utilsateur ou leur id is idUser 
+	 */
+	User findUserById(Long idUser);
 
 }
--- src/main/java/com/ensialligence/web/rest/UserResource.java
+++ src/main/java/com/ensialligence/web/rest/UserResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -27,10 +28,18 @@ public class UserResource {
     public User getUser(@QueryParam(value="username") String username,@QueryParam(value="password") String password) {
         return userService.findUser(username, password);
     }
+    
+    @GET
+    @Path("/listFriends")
+    @Produces("application/json")
+    public ArrayList<User> getListFriends(@QueryParam(value="id") Long id) {
+    	System.out.println(id);
+        return userService.listFriends(id);
+    }
 
     @POST
     @Path("/addUser")
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
     @Consumes({"application/x-www-form-urlencoded","multipart/form-data",MediaType.APPLICATION_JSON})
     public boolean addUser(@FormParam("username") String username,
     					   @FormParam("password") String password,
